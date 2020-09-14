6863b3e91ca410ff3fb9a880374758608ad6e367
Alex <alex.ewers84@yahoo.co.uk>
2020-09-14 01:57:12 +0100

    Build X

---
+++ Assets/Code/Kill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+
+    //Kill and Respawning
+    [SerializeField] Transform spawnpoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = spawnpoint.position;
+    }
+}
