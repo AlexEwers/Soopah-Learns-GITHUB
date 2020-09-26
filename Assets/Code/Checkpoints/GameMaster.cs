3452d1e91709d875558db79c603be45afda72d1d
Alex <alex.ewers84@yahoo.co.uk>
2020-09-26 02:21:37 +0100

    Checkpoint code should be added now

---
+++ Assets/Code/Checkpoints/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster instance { get; private set; }
+    public Vector2 lastCheckPointPos;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Code/Checkpoints/PlayerPos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+    private Rigidbody2D Rigidbody;
+
+    private void Start()
+    {
+        
+        gm = GameMaster.instance;
+        Rigidbody = GetComponent<Rigidbody2D>();
+        Debug.Log($"lastCheckPointPos = {gm.lastCheckPointPos}");
+        Rigidbody.position = gm.lastCheckPointPos;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+    }
+}
+
+    
